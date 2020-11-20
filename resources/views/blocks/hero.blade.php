a56ad3458bfc175c69af705b2c9cb5ec7d754c68
edward01s2 <edward.01s2@gmail.com>
2020-11-20 08:30:48 -0600

    Add apple badge

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -8,9 +8,8 @@
             <svg class="w-6 h-6 mr-2 text-white fill-current md:w-7 md:h-7" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>YouTube icon</title><path d="M23.495 6.205a3.007 3.007 0 0 0-2.088-2.088c-1.87-.501-9.396-.501-9.396-.501s-7.507-.01-9.396.501A3.007 3.007 0 0 0 .527 6.205a31.247 31.247 0 0 0-.522 5.805 31.247 31.247 0 0 0 .522 5.783 3.007 3.007 0 0 0 2.088 2.088c1.868.502 9.396.502 9.396.502s7.506 0 9.396-.502a3.007 3.007 0 0 0 2.088-2.088 31.247 31.247 0 0 0 .5-5.783 31.247 31.247 0 0 0-.5-5.805zM9.609 15.601V8.408l6.264 3.602z"/></svg>
             <span class="mt-1">{!! $youtube['title'] !!}</span>
           </a>
-          <a class="inline-flex items-center justify-center w-48 px-6 py-3 text-black transition duration-300 bg-white hover:opacity-75" href="{!! $apple['url'] !!}" target="_blank">
-            <img class="w-6 h-6 mr-2" src="@asset('images/podcast.png')" alt="">
-            <span class="mt-1">{!! $apple['title'] !!}</span>
+          <a class="inline-flex items-center justify-center transition duration-300 hover:opacity-75" href="{!! $apple['url'] !!}" target="_blank">
+            <img class="w-48 h-auto" src="@asset('images/apple_badge.svg')" alt="">
           </a>
         </div>
       </div>
